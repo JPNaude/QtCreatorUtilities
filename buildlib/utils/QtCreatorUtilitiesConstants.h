e98d59441277b876dea2d97b52e592993554da37
Jaco Naude <naude.jaco@gmail.com>
2013-01-24 10:42:38 +0200

    Adding missing QtCreatorUtilitiesConstants.h

---
+++ buildlib/utils/QtCreatorUtilitiesConstants.h
@@ -0,0 +1,23 @@
+#ifndef QT_CREATOR_UTILITIES_CONSTANTS_H
+#define QT_CREATOR_UTILITIES_CONSTANTS_H
+
+#include <QObject>
+#include <ScinericWorkspace.h>
+
+namespace DesignCore {
+    namespace Constants {
+        const char * const QT_CREATOR_ICON_ARROW_DOWN = ":/images/arrowdown.png";
+        const char * const QT_CREATOR_ICON_ARROW_UP = ":/images/arrowup.png";
+        const char * const QT_CREATOR_ICON_BUTTON_CLOSE = ":/images/button_close.png";
+        const char * const QT_CREATOR_ICON_CLEAR  = ":/images/clear.png";
+        const char * const QT_CREATOR_ICON_BUTTON_CLOSE_LIGHT = ":/images/closebutton.png";
+        const char * const QT_CREATOR_ICON_DARK_ARROW_DOWN = ":/images/darkarrowdown.png";
+        const char * const QT_CREATOR_ICON_DARK_ARROW_UP = ":/images/darkarrowup.png";
+        const char * const QT_CREATOR_ICON_DARK_BUTTON_CLOSE = ":/images/darkclose.png";
+        const char * const QT_CREATOR_ICON_DARK_BUTTON_CLOSE_LIGHT = ":/images/darkclosebutton.png";
+        const char * const QT_CREATOR_ICON_DISABLED = ":/images/disabled.png";
+        const char * const QT_CREATOR_ICON_SETTINGS = ":/images/settings.png";
+    }
+}
+
+#endif // QT_CREATOR_UTILITIES_CONSTANTS_H
